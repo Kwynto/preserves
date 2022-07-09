c74c19ac28569609872e0bf9d6e00ff972ee480f
Constantine Zavezeon (Kwynto) <kwynto@mail.ru>
2022-07-09 19:14:32 +0300

    FindEmail()

--- ordinary.go
+++ ordinary.go
@@ -2,7 +2,9 @@ package preserves
 
 import (
 	cryptoRand "crypto/rand"
+	"errors"
 	"math/big"
+	"regexp"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,6 +13,16 @@ import (
 func RandInt(min, max int64) int64 {
 	nBig, _ := cryptoRand.Int(cryptoRand.Reader, big.NewInt(max-min))
 	return nBig.Int64() + min
+}
+
+// The FindEmail() function looks for an email address in a string and returns it or an error.
+func FindEmail(input string) (string, error) {
+	emailRegexp := regexp.MustCompile(`.+@.+\..+`)
+	first := emailRegexp.FindString(input)
+	if first == "" {
+		return "", errors.New("email address not found")
+	}
+	return first, nil
 }
 
 // func OpenDB(dsn string) (*sql.DB, error) {
--- ordinary_test.go
+++ ordinary_test.go
@@ -28,6 +28,53 @@ func Test_RandInt(t *testing.T) {
 	}
 }
 
+func Test_FindEmail(t *testing.T) {
+	type args struct {
+		val string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Email search 1",
+			args: args{
+				val: "email@example.com",
+			},
+			want: "email@example.com",
+		},
+		{
+			name: "Email search 2",
+			args: args{
+				val: "other@domain.com",
+			},
+			want: "other@domain.com",
+		},
+		{
+			name: "Email search 3",
+			args: args{
+				val: "other-mail@other-domain.xyz",
+			},
+			want: "other-mail@other-domain.xyz",
+		},
+		{
+			name: "Email search 4",
+			args: args{
+				val: "other-mail&@other-!(domain)",
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := FindEmail(tt.args.val); got != tt.want {
+				t.Errorf("FindEmail() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 // func Test_OpenDB(t *testing.T) {
 // 	type args struct {
 // 		dsn string
@@ -70,6 +117,12 @@ func Test_RandInt(t *testing.T) {
 func Benchmark_RandInt(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = RandInt(0, int64(i+1)) // calling the tested function
+	}
+}
+
+func Benchmark_FindEmail(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = FindEmail("email@example.com") // calling the tested function
 	}
 }
 
